331900032c616006a2c3184cfa59c6b642c02d82
tesfa <tesfayegirma360@gmail.com>
2022-12-16 23:06:08 +0300

    fix bug with update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
+module.exports = async function (app) {
+  await app.use(
     "/api",
     createProxyMiddleware({
       target: "https://essethr.herokuapp.com",
